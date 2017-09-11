0fd6a24f4dd0ed777ecf9d5106670f1874a23cf2
Fred Honorio <fredshonorio@gmail.com>
2017-09-11 22:46:08 +0100

    Added more utility methods to NParamMap

--- src/main/java/com/fredhonorio/neu/type/NParamMap.java
+++ src/main/java/com/fredhonorio/neu/type/NParamMap.java
@@ -1,8 +1,8 @@
 package com.fredhonorio.neu.type;
 
 import javaslang.Tuple2;
-import javaslang.collection.Map;
 import javaslang.collection.TreeMap;
+import javaslang.control.Option;
 
 import java.util.function.Function;
 
@@ -13,12 +13,60 @@ public class NParamMap implements Parameter {
         this.value = value;
     }
 
-    public NParamMap put(String key, Parameter value) {
-        return new NParamMap(this.value.put(key, value));
+    public NParamMap put(String k, Parameter v) {
+        return new NParamMap(value.put(k, v));
+    }
+
+    public NParamMap put(String k, boolean v) {
+        return new NParamMap(value.put(k, Value.nBoolean(v)));
+    }
+
+    public NParamMap put(String k, String v) {
+        return new NParamMap(value.put(k, Value.nString(v)));
+    }
+
+    public NParamMap put(String k, double v) {
+        return new NParamMap(value.put(k, Value.nFloat(v)));
+    }
+
+    public NParamMap put(String k, long v) {
+        return new NParamMap(value.put(k, Value.nInteger(v)));
+    }
+
+    public NParamMap putIfDefined(String k, Option<? extends Parameter> prop) {
+        return prop.map(p -> put(k, p)).getOrElse(this);
+    }
+
+    public <T> NParamMap putIfDefined(String k, Option<T> prop, Function<T, Parameter> converter) {
+        return prop.map(p -> put(k, converter.apply(p))).getOrElse(this);
+    }
+
+    public static NParamMap of(String k, Parameter v) {
+        return new NParamMap(TreeMap.of(k, v));
+    }
+
+    public static NParamMap of(String k, boolean v) {
+        return new NParamMap(TreeMap.of(k, Value.nBoolean(v)));
+    }
+
+    public static NParamMap of(String k, String v) {
+        return new NParamMap(TreeMap.of(k, Value.nString(v)));
+    }
+
+    public static NParamMap of(String k, double v) {
+        return new NParamMap(TreeMap.of(k, Value.nFloat(v)));
+    }
+
+    public static NParamMap of(String k, long v) {
+        return new NParamMap(TreeMap.of(k, Value.nInteger(v)));
     }
 
     public static NParamMap empty() {
         return new NParamMap(TreeMap.empty());
+    }
+
+    public static NParamMap of(Iterable<Tuple2<String, Parameter>> map) {
+        return new NParamMap(TreeMap.ofEntries(map));
     }
 
     @Override
@@ -39,10 +87,6 @@ public class NParamMap implements Parameter {
         NParamMap nParamMap = (NParamMap) o;
 
         return value != null ? value.equals(nParamMap.value) : nParamMap.value == null;
-    }
-
-    public static NParamMap of(Iterable<Tuple2<String, Parameter>> map) {
-        return new NParamMap(TreeMap.ofEntries(map));
     }
 
     @Override
